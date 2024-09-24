2397f369d4214d2bb56b67494ffd44799273e7dc
vicct0r <v1ctor_inacio@proton.me>
2024-09-24 18:05:05 -0300

    Mudando STORAGES

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -19,21 +19,18 @@ DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-AWS_DEFAULT_ACL = None 
-AWS_S3_REGION_NAME = 'sa-east-1'
-AWS_S3_SIGNATURE_VERSION = 's3v4' 
+AWS_S3_FILE_OVERWRITE = False
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400'
-}
-AWS_LOCATION = 'static'
-AWS_QUERYSTRING_AUTH = False
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
-# Configure STATIC_ROOT
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
+    }
+}
+
 
 # Heroku Loggin
 DEBUG_PROPAGATE_EXCEPTIONS = True
