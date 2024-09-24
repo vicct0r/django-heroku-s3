17c60cb6e70bba5ca814920ac4ae671ccde0802d
vicct0r <v1ctor_inacio@proton.me>
2024-09-24 17:37:56 -0300

    Preparando para produção

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,33 +1,36 @@
 from .base import *
 import django_heroku
+import dj_database_url
+import os
 
 # CONFIGURAÇÕES PARA DESENVOLVIMENTO EM PRODUÇÃO
 
-SECRET_KEY = 'django-insecure-za8lbt!ddvm^h@v)g)#7-2$%ttbd!f-nmvh3+jpt2lhm_3w-y*'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-# Amazon S3 Configuração
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY_ID')
-AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+# heroku database settings
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+# AWS S3 SETTINGS
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_DEFAULT_ACL = None 
+AWS_S3_REGION_NAME = 'sa-east-1'
+AWS_S3_SIGNATURE_VERSION = 's3v4' 
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400'
 }
 AWS_LOCATION = 'static'
 AWS_QUERYSTRING_AUTH = False
-AWS_HEADERS = {
-    'Access-Control-Allow-Origin': '*'
-}
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
-
 
 # Heroku Loggin
 DEBUG_PROPAGATE_EXCEPTIONS = True
@@ -62,5 +65,5 @@ LOGGING = {
 
 
 # Heroku settings
-django_heroku.settings(locals())
-del DATABASES['default']['OPTIONS']['sslmode']
+django_heroku.settings(locals(), staticfiles=False)
+
